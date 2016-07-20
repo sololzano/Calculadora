568b97e3b5c0474bf7fe5688da536713929b136a
ivanh96 <ivanh96@hotmail.com>
2016-07-20 09:11:49 -0600

    Add files via upload

---
+++ Calculadora.java
@@ -0,0 +1,55 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Calculadora {
+
+    /**
+     * 
+     */
+    public Calculadora() {
+    }
+
+    /**
+     * @param num1 
+     * @param num2 
+     * @return
+     */
+    public T sumar(T num1, T num2) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param num1 
+     * @param num2 
+     * @return
+     */
+    public T resta(T num1, T num2) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param num1 
+     * @param num2 
+     * @return
+     */
+    public T multimplicar(T num1, T num2) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param num1 
+     * @param num2 
+     * @return
+     */
+    public T dividir(T num1, T num2) {
+        // TODO implement here
+        return null;
+    }
+
+}
